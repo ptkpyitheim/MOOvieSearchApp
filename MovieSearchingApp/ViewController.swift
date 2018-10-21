3387d4da3b33c6bbd4d8d065e0741a52c4391d3d
Pyi Theim Kyaw <k.pyitheim@wustl.edu>
2018-10-20 20:35:20 -0500

    Initial Commit

---
+++ MovieSearchingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieSearchingApp
+//
+//  Created by Pyi Theim Kyaw on 10/20/18.
+//  Copyright © 2018 Pyi Theim Kyaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
